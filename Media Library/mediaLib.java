9ba719e3a35b51fe7bd30aeeea5d537df7eb16e4
Amogh Joshi <67437306+amogh7joshi@users.noreply.github.com>
2020-10-14 18:24:24 -0400

    Create mediaLib.java

---
+++ Media Library/mediaLib.java
@@ -0,0 +1,11 @@
+package medialib;
+
+public class mediaLib {
+	
+	public static void main(String[] args) {
+		Song pc2 = new Song("Piano Concerto #2", "Sergei Rachmanioff", 10, true);
+		Song lc = new Song("La Campanella", "Franz Liszt", 10, true);
+		
+	}
+
+}
