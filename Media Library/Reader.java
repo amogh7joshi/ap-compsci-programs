3f6fdc696399658e6cc17d1f3ca043fb691da82c
Amogh Joshi <67437306+amogh7joshi@users.noreply.github.com>
2020-10-20 18:18:02 -0400

    Update Reader.java

--- Media Library/Reader.java
+++ Media Library/Reader.java
@@ -1,7 +1,11 @@
+package mediaLib;
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public final class Reading {
 	
@@ -38,10 +42,43 @@ public final class Reading {
 					Double.parseDouble(strings[t+3]), Boolean.parseBoolean(strings[t+4]));
 		}
 		
-		System.out.println(Arrays.toString(songs));
+		System.out.println(Arrays.toString(songs));	
+		
+		System.out.println(avgPrice(songs));
+		
+		System.out.println(avgRating(songs));
 		
 		
 		
 	}
+	
+	public static double avgPrice(Song songs[]) {
+		double total = 0;
+		for(Song song: songs) {
+			total += song.getPrice();
+		}
+		
+		return round(total / songs.length, 2);
+	}
+	
+	public static double avgRating(Song songs[]) {
+		double total = 0;
+		for(Song song: songs) {
+			total += song.getRating();
+		}
+		
+		return round(total / songs.length, 2);
+	}
+	
+	public static double round(double value, int places) {
+		if (places < 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		BigDecimal decimal = new BigDecimal(value);
+		decimal = decimal.setScale(places, RoundingMode.HALF_UP);
+		return decimal.doubleValue();
+	}
+	
 
 }
